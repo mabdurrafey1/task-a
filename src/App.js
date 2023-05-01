181a3011abe920874d9a6ea9a85ab84b33cd990e
mabdurrafey <mabdurrafey1@gmail.com>
2023-05-01 23:30:16 +0500

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { element, func } from "prop-types";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      text: "hello",
+      priority: 0,
+    },
+    {
+      text: "hi",
+      priority: 0,
+    },
+  ]);
+  const [inputValue, setInputValue] = useState("");
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  function addNewItem(text) {
+    const newItems = [...items, { text, priority: 0 }];
+    setItems(newItems);
+    setInputValue("");
+  }
+
+  function handleDeleteClick(id) {
+    const newItems = [...items];
+    newItems.splice(id, 1);
+    setItems(newItems);
+  }
+  function increasePriority(id) {
+    const newItems = [...items];
+    newItems[id].priority += 1;
+    setItems(newItems);
+  }
+  function decreasePriority(id) {
+    const newItems = [...items];
+    newItems[id].priority -= 1;
+    setItems(newItems);
+  }
+  function sortItems() {
+    console.log(1);
+    const newItems = [...items];
+    newItems.sort((b, a) => a.priority - b.priority);
+    setItems(newItems);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <br></br>
+      <input
+        placeholder="Enter Items"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button
+        onClick={() => {
+          addNewItem(inputValue);
+        }}
+      >
+        Enter
+      </button>
+      <ul>
+        {items.map((element, index) => {
+          return (
+            <li key={index}>
+              <span>{element.text}</span>
+              <button onClick={() => handleDeleteClick(index)}>
+                {" "}
+                <i className="fas fa-trash"></i>{" "}
+              </button>
+              <button
+                onClick={() => {
+                  increasePriority(index);
+                }}
+              >
+                <i className="fas fa-arrow-up"></i>
+              </button>
+              <span>{element.priority}</span>
+              <button
+                onClick={() => {
+                  decreasePriority(index);
+                }}
+              >
+                <i className="fas fa-arrow-down"></i>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      <button onClick={() => sortItems()}>Sort</button>
     </div>
   );
 }
